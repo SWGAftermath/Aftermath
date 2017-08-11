ceca5db39bc68f07b68a357a6dc3592928c4f955
c0pp3r <dominguez.joey@gmail.com>
2017-08-11 08:58:03 -0400

    250 items per lot

--- MMOCoreORB/src/server/zone/objects/building/BuildingObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/building/BuildingObjectImplementation.cpp
@@ -796,7 +796,7 @@ uint32 BuildingObjectImplementation::getMaximumNumberOfPlayerItems() {
 
 	auto maxItems = MAXPLAYERITEMS;
 
-	return Math::min(maxItems, lots * 100);
+	return Math::min(maxItems, lots * 250);
 }
 
 int BuildingObjectImplementation::notifyObjectInsertedToChild(SceneObject* object, SceneObject* child, SceneObject* oldParent) {
