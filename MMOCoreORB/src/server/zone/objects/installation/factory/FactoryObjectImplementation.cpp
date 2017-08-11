e680e7512ffcae702e9879104fa232e86359f6f3
c0pp3r <dominguez.joey@gmail.com>
2017-08-11 09:21:55 -0400

    Factory speed to aftermath values

--- MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/installation/factory/FactoryObjectImplementation.cpp
@@ -415,7 +415,7 @@ bool FactoryObjectImplementation::startFactory() {
 			return false;
 	}
 
-	timer = ((int)schematic->getComplexity()) * 8;
+	timer = ((int)schematic->getComplexity()) * 4;
 
 	if(!populateSchematicBlueprint(schematic))
 		return false;
