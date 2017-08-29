80c8ab249c4b4577599c62feb08f5685b00591ff
c0pp3r <dominguez.joey@gmail.com>
2017-08-29 08:35:43 -0400

    Adjust base armor segments to match non-adv composite segment

--- MMOCoreORB/bin/scripts/object/tangible/component/armor/armor_segment_bone.lua
+++ MMOCoreORB/bin/scripts/object/tangible/component/armor/armor_segment_bone.lua
@@ -48,9 +48,9 @@ object_tangible_component_armor_armor_segment_bone = object_tangible_component_a
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "null", "exp_quality", "exp_durability", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
 	experimentalSubGroupTitles = {"null", "null", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
-	experimentalMin = {0, 0, 1000, 1, 100, 6, 8, 4, 1, 2, 1, 100},
-	experimentalMax = {0, 0, 1000, 5, 1000, 1, 1, 1, 1, 2, 10, 1000},
-	experimentalPrecision = {0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 10, 0},
+	experimentalMin = {0, 0, 1000, 1, 100, 13, 13, 16, 1, 256, 1, 100},
+	experimentalMax = {0, 0, 1000, 10, 1000, 1, 1, 1, 1, 256, 15, 1000},
+	experimentalPrecision = {0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
 	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
 
--- MMOCoreORB/bin/scripts/object/tangible/component/armor/armor_segment_zam.lua
+++ MMOCoreORB/bin/scripts/object/tangible/component/armor/armor_segment_zam.lua
@@ -48,10 +48,10 @@ object_tangible_component_armor_armor_segment_zam = object_tangible_component_ar
 	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "null", "exp_quality", "exp_quality", "exp_durability", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
 	experimentalSubGroupTitles = {"null", "null", "hit_points", "quality", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
-	experimentalMin = {0, 0, 1000, 1, 1, 100, 6, 8, 4, 1, 32, 1, 100},
-	experimentalMax = {0, 0, 1000, 40, 5, 1000, 1, 1, 1, 1, 32, 7, 1000},
-	experimentalPrecision = {0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
-	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
+	experimentalMin = {0, 0, 1000, 1, 100, 13, 13, 16, 1, 256, 1, 100},
+	experimentalMax = {0, 0, 1000, 10, 1000, 1, 1, 1, 1, 256, 15, 1000},
+	experimentalPrecision = {0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
+	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
 
 ObjectTemplates:addTemplate(object_tangible_component_armor_armor_segment_zam, "object/tangible/component/armor/armor_segment_zam.iff")
