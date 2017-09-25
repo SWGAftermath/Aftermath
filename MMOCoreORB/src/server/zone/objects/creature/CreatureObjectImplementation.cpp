6eaeae7d7eadbeaf6b152246830dcbd462989c22
c0pp3r <dominguez.joey@gmail.com>
2017-09-25 08:27:06 -0400

    [fixed] stability issue

--- MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
@@ -114,8 +114,8 @@ void CreatureObjectImplementation::initializeMembers() {
 	linkedCreature = NULL;
 	controlDevice = NULL;
 
-	bankCredits = -1;
-	cashCredits = -1;
+	bankCredits = 0;
+	cashCredits = 0;
 
 	pvpStatusBitmask = 0;
 
@@ -3508,8 +3508,8 @@ CreditObject* CreatureObjectImplementation::getCreditObject() {
 		creditObject->setBankCredits(bankCredits, false);
 		creditObject->setCashCredits(cashCredits, false);
 		creditObject->setOwner(_this.getReferenceUnsafeStaticCast());
-		cashCredits = -1;
-		bankCredits = -1;
+		cashCredits = 0;
+		bankCredits = 0;
 	}
 
 	return creditObject;
