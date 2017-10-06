39096d9e2ace3be18024c1329339906ff6eb6bd7
c0pp3r <dominguez.joey@gmail.com>
2017-10-05 22:15:59 -0400

    Reduce the powers of CSR

--- MMOCoreORB/bin/scripts/staff/levels/csr.lua
+++ MMOCoreORB/bin/scripts/staff/levels/csr.lua
@@ -7,15 +7,7 @@ csr = {
 		"admin_base",
 		"admin_debug_01",
 		"admin_general_admin_01",
-		"admin_general_admin_02",
-		"admin_jedi_management_01",
-		"admin_player_management_01",
-		"admin_player_management_02",
-		"admin_player_management_03",
-		"admin_player_management_04",
-		"admin_quest_management_01",
-		"admin_server_admin_01",
-		"admin_spawn_management_01"
+		"admin_general_admin_02"
 	}
 }
 
