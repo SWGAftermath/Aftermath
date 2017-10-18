259b6e3807a72412265cd843184403f49e57d334
c0pp3r <dominguez.joey@gmail.com>
2017-10-18 11:03:51 -0400

    attempt to fix revoke skill command

--- MMOCoreORB/src/server/zone/objects/creature/commands/RevokeSkillCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RevokeSkillCommand.h
@@ -21,6 +21,38 @@ public:
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
 
+		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
+
+		if (object == NULL || !object->isCreatureObject())
+			return INVALIDTARGET;
+
+		CreatureObject* targetCreature = cast<CreatureObject*>( object.get());
+
+		Locker clocker(targetCreature, creature);
+
+		SkillManager* skillManager = SkillManager::instance();
+		bool skillRevoked = skillManager->surrenderSkill(arguments.toString(), targetCreature, true);
+
+		if (skillRevoked) {
+			StringIdChatParameter params;
+			params.setTO(arguments.toString());
+			params.setStringId("skill_teacher", "skill_terminal_grant");
+
+			targetCreature->sendSystemMessage(params);
+
+			creature->sendSystemMessage("Revoked skill " + arguments.toString()
+					+ "to " + targetCreature->getFirstName());
+		} else {
+			StringIdChatParameter params;
+			params.setTO(arguments.toString());
+			params.setStringId("skill_teacher", "prose_train_failed");
+
+			targetCreature->sendSystemMessage(params);
+
+			creature->sendSystemMessage("Failed to revoke skill " + arguments.toString()
+					+ "to " + targetCreature->getFirstName());
+		}
+
 		return SUCCESS;
 	}
 
