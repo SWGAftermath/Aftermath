bee04e041e9ec060fdd9f341919eedfefb51b6ae
c0pp3r <dominguez.joey@gmail.com>
2017-10-22 14:17:52 -0400

    dxr6 carbine to heavy AP

--- MMOCoreORB/bin/scripts/object/weapon/ranged/carbine/carbine_dxr6.lua
+++ MMOCoreORB/bin/scripts/object/weapon/ranged/carbine/carbine_dxr6.lua
@@ -72,7 +72,7 @@ object_weapon_ranged_carbine_carbine_dxr6 = object_weapon_ranged_carbine_shared_
 	damageType = ACID,
 
 	-- NONE, LIGHT, MEDIUM, HEAVY
-	armorPiercing = MEDIUM,
+	armorPiercing = HEAVY,
 
 	-- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
 	-- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
