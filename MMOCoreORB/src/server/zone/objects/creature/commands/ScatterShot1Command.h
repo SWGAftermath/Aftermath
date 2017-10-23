e92f9210b4ec57f8ebb3192ad8ae681957309737
c0pp3r <dominguez.joey@gmail.com>
2017-10-23 12:26:31 -0400

    Change scattershot1 to mimick new scattershot2 with lower damage

--- MMOCoreORB/src/server/zone/objects/creature/commands/ScatterShot1Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ScatterShot1Command.h
@@ -21,15 +21,8 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
-		
-		float mods[3] = {0.f, 0.f, 0.f};
-		
-		for (int i = 0; i < 2; i++)
-			mods[System::random(2)] += 0.5f;
-			
-		UnicodeString args = "healthDamageMultiplier=" + String::valueOf(mods[0]) + ";actionDamageMultiplier=" + String::valueOf(mods[1]) + ";mindDamageMultiplier=" + String::valueOf(mods[2]) + ";";
 
-		return doCombatAction(creature, target, args);
+		return doCombatAction(creature, target);
 	}
 
 };
