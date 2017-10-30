43ccb03beb31b206ad0c2818bf5704883992f687
c0pp3r <dominguez.joey@gmail.com>
2017-10-29 23:57:28 -0400

    Allow self-stat migration outside of tutorial zone

--- MMOCoreORB/src/server/zone/objects/creature/commands/RequestSetStatMigrationDataCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/RequestSetStatMigrationDataCommand.h
@@ -85,7 +85,7 @@ public:
 		//Player is in the tutorial zone and is allowed to migrate stats.
 		Zone* zone = creature->getZone();
 
-		if (zone != NULL && zone->getZoneName() == "tutorial")
+		if (zone != NULL)
 			session->migrateStats();
 
 
