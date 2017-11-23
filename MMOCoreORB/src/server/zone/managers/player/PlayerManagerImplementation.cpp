7aca09ca43e0c49c031aaa2f545848fe189f614e
c0pp3r <dominguez.joey@gmail.com>
2017-11-22 22:34:33 -0500

    2x XP Thanksgiving weekend

--- MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -1615,7 +1615,7 @@ int PlayerManagerImplementation::awardExperience(CreatureObject* player, const S
 		xpType == "music" || 
 		xpType == "dance" ||
 		xpType == "entertainer_healing"){
-			xp = playerObject->addExperience(xpType, (amount * 5));
+			xp = playerObject->addExperience(xpType, (amount * 6));
 			float speciesModifier = 1.f;
 
 			if (amount > 0)
