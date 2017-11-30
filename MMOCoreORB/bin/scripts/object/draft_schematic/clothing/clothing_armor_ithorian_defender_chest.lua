e40d7adf4401ba982e89cda336929f0409f7ac70
Dirtnose <dirtnose@gmail.com>
2017-11-30 16:02:25 +0000

    itho defender to match itho sentinel

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_ithorian_defender_chest.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_ithorian_defender_chest.lua
@@ -48,7 +48,7 @@ object_draft_schematic_clothing_clothing_armor_ithorian_defender_chest = object_
    customObjectName = "Ithorian Defender Chest Plate",
 
    craftingToolTab = 2, -- (See DraftSchematicObjectTemplate.h)
-   complexity = 40, 
+   complexity = 45, 
    size = 4, 
 
    xpType = "crafting_clothing_armor", 
@@ -65,8 +65,8 @@ object_draft_schematic_clothing_clothing_armor_ithorian_defender_chest = object_
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"auxilary_coverage", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
    ingredientSlotType = {0, 0, 0, 0, 0, 0, 1, 1, 1},
-   resourceTypes = {"hide_leathery_lok", "hide_scaley", "fiberplast_corellia", "metal", "petrochem_inert_polymer", "hide_wooly", "object/tangible/component/armor/shared_armor_segment_padded.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
-   resourceQuantities = {80, 80, 40, 50, 50, 50, 4, 1, 1},
+   resourceTypes = {"ore_intrusive", "fuel_petrochem_solid_known", "fiberplast_naboo", "aluminum", "copper_beyrllius", "hide_wooly", "object/tangible/component/armor/shared_armor_segment_composite.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {100, 100, 50, 60, 50, 40, 4, 1, 1},
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
