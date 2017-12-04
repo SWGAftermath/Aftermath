cedbe7506e83b29858ad269346b7ccf5d5634daa
Dirtnose <dirtnose@gmail.com>
2017-12-04 14:12:16 +0000

    Black Mountain armor to match Hunting armor

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_kashyyykian_black_mtn_bracer_r.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_kashyyykian_black_mtn_bracer_r.lua
@@ -48,7 +48,7 @@ object_draft_schematic_clothing_clothing_armor_kashyyykian_black_mtn_bracer_r = 
    customObjectName = "Kashyyykian Black Mountain Right Bracer",
 
    craftingToolTab = 2, -- (See DraftSchematicObjectTemplate.h)
-   complexity = 40, 
+   complexity = 45, 
    size = 4, 
 
    xpType = "crafting_clothing_armor", 
@@ -65,8 +65,8 @@ object_draft_schematic_clothing_clothing_armor_kashyyykian_black_mtn_bracer_r = 
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"auxilary_coverage", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
    ingredientSlotType = {0, 0, 0, 0, 0, 1, 1, 1, 0},
-   resourceTypes = {"wood_deciduous_endor", "hide_leathery", "hide_wooly_naboo", "copper", "petrochem_inert_polymer", "object/tangible/component/clothing/shared_padding_segment.iff", "object/tangible/component/armor/shared_armor_segment_kashyyykian_black_mtn.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "fiberplast"},
-   resourceQuantities = {40, 40, 30, 25, 25, 2, 2, 1, 20},
+   resourceTypes = {"softwood_evergreen_dathomir", "hide_leathery_lok", "hide_wooly_tatooine", "copper_mythra", "petrochem_inert_polymer", "object/tangible/component/clothing/shared_padding_segment.iff", "object/tangible/component/armor/shared_armor_segment_kashyyykian_hunting.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "fiberplast_naboo"},
+   resourceQuantities = {50, 50, 40, 20, 30, 4, 2, 1, 30},
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
