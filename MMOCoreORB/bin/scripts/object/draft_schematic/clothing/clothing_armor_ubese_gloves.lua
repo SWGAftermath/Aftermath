36bf2fdecba2c0233394cf44a1b74fb3bdc910b4
Dirtnose <dirtnose@gmail.com>
2017-12-11 15:44:25 +0000

    Ubese mats to match comp. Jacket and gloves increased to make up for lack of biceps

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_ubese_gloves.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_ubese_gloves.lua
@@ -48,7 +48,7 @@ object_draft_schematic_clothing_clothing_armor_ubese_gloves = object_draft_schem
    customObjectName = "Ubese Armor Gloves",
 
    craftingToolTab = 2, -- (See DraftSchematicObjectTemplate.h)
-   complexity = 35, 
+   complexity = 45, 
    size = 4, 
 
    xpType = "crafting_clothing_armor", 
@@ -65,8 +65,8 @@ object_draft_schematic_clothing_clothing_armor_ubese_gloves = object_draft_schem
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"auxilary_coverage", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
    ingredientSlotType = {0, 0, 0, 0, 0, 0, 1, 1, 1},
-   resourceTypes = {"iron_doonium", "steel", "hide_leathery", "steel_neutronium", "petrochem_inert_polymer", "hide_wooly_tatooine", "object/tangible/component/armor/shared_armor_segment_ubese.iff", "object/tangible/component/clothing/shared_fiberplast_panel.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
-   resourceQuantities = {20, 20, 20, 10, 10, 10, 1, 1, 1},
+   resourceTypes = {"iron_doonium", "steel", "hide_leathery", "steel_neutronium", "petrochem_inert_polymer", "hide_wooly_tatooine", "object/tangible/component/armor/shared_armor_segment_ubese.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {75, 75, 40, 40, 30, 30, 1, 2, 1},
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_ubese_jacket.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_ubese_jacket.lua
@@ -48,7 +48,7 @@ object_draft_schematic_clothing_clothing_armor_ubese_jacket = object_draft_schem
    customObjectName = "Ubese Armor Jacket",
 
    craftingToolTab = 2, -- (See DraftSchematicObjectTemplate.h)
-   complexity = 35, 
+   complexity = 45, 
    size = 4, 
 
    xpType = "crafting_clothing_armor", 
@@ -65,8 +65,8 @@ object_draft_schematic_clothing_clothing_armor_ubese_jacket = object_draft_schem
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"auxilary_coverage", "body", "liner", "hardware_and_attachments", "binding_and_reinforcement", "padding", "armor", "load_bearing_harness", "reinforcement"},
    ingredientSlotType = {0, 0, 0, 0, 0, 0, 1, 1, 1},
-   resourceTypes = {"iron_doonium", "steel", "hide_leathery", "steel_neutronium", "petrochem_inert_polymer", "hide_wooly_tatooine", "object/tangible/component/armor/shared_armor_segment_ubese.iff", "object/tangible/component/clothing/shared_fiberplast_panel.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
-   resourceQuantities = {50, 50, 50, 30, 30, 30, 4, 1, 1},
+   resourceTypes = {"iron_doonium", "steel", "hide_leathery", "steel_neutronium", "petrochem_inert_polymer", "hide_wooly_tatooine", "object/tangible/component/armor/shared_armor_segment_ubese.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {150, 150, 75, 90, 70, 60, 4, 1, 1},
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
