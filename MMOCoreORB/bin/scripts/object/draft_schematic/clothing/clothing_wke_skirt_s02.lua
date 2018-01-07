e87ff1b6f15f14e0f2e193829120289f23a053c6
c0pp3r <dominguez.joey@gmail.com>
2018-01-06 22:29:26 -0500

    Give wookie 2 new pieces of BE clothing

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_wke_skirt_s02.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_wke_skirt_s02.lua
@@ -65,8 +65,8 @@ object_draft_schematic_clothing_clothing_wke_skirt_s02 = object_draft_schematic_
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"body", "binding_and_reinforcement", "hardware", "lining"},
    ingredientSlotType = {1, 0, 1, 1},
-   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "petrochem_inert", "object/tangible/component/clothing/shared_metal_fasteners.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
-   resourceQuantities = {1, 15, 1, 1},
+   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "petrochem_inert", "object/tangible/component/clothing/shared_metal_fasteners.iff", "object/tangible/component/clothing/shared_reinforced_fiber_panels.iff"},
+   resourceQuantities = {1, 15, 1, 2},
    contribution = {100, 100, 100, 100},
 
 
