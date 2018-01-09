ce9f01e769a20cb4b5d7147b5885b04dfb9ab92b
c0pp3r <dominguez.joey@gmail.com>
2018-01-09 07:15:06 -0500

    Ranger track increased to 2048 from 512.

--- MMOCoreORB/src/server/zone/objects/player/events/AreaTrackTask.h
+++ MMOCoreORB/src/server/zone/objects/player/events/AreaTrackTask.h
@@ -69,8 +69,8 @@ public:
 			bool canGetDirection = player->hasSkill("outdoors_ranger_harvest_01");
 		    bool canGetDistance = player->hasSkill("outdoors_ranger_harvest_03");
 
-			SortedVector<ManagedReference<QuadTreeEntry*> > objects(512, 512);
-			zone->getInRangeObjects(player->getPositionX(), player->getPositionY(), 512, &objects, true);
+			SortedVector<ManagedReference<QuadTreeEntry*> > objects(2048, 2048);
+			zone->getInRangeObjects(player->getPositionX(), player->getPositionY(), 2048, &objects, true);
 
 			for (int i = 0; i < objects.size(); ++i) {
 				SceneObject* object = static_cast<SceneObject*>(objects.get(i).get());
