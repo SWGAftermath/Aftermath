1ba7ebe9b528d426a305a9d3ec36eb0115621b10
c0pp3r <dominguez.joey@gmail.com>
2018-01-11 23:00:46 -0500

    add debug logging for MBH reduction

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -843,7 +843,9 @@ float CombatManager::getDefenderToughnessModifier(CreatureObject* defender, int 
 		damage *= 1.f - (jediToughness / 100.f);
 
 	if (damType == SharedWeaponObjectTemplate::LIGHTSABER && defender->isPlayerCreature() && defender->hasSkill("combat_bountyhunter_master")){
+		info("Damage after BH Reduction: " + String::valueOf(damage), true);
 		damage *= 1.f - (20.f/100.f);
+		info("Damage after BH Reduction: " + String::valueOf(damage), true);
 	}
 
 	return damage < 0 ? 0 : damage;
