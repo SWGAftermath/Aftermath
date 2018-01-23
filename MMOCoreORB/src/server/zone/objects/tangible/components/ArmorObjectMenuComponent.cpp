75120dce490cc1af534a386522f08f7559ce2e31
c0pp3r <dominguez.joey@gmail.com>
2018-01-23 11:55:16 -0500

    Revert "add frame color to radial for armor"
    This reverts commit c99869bb9785cbb9afe20cf78ccd988a212eb9d9.

--- MMOCoreORB/src/server/zone/objects/tangible/components/ArmorObjectMenuComponent.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/components/ArmorObjectMenuComponent.cpp
@@ -41,14 +41,13 @@ void ArmorObjectMenuComponent::fillObjectMenuResponse(SceneObject* sceneObject, 
 
 	String text = "Color Change";
 	menuResponse->addRadialMenuItem(81, 3, text);
-	menuResponse->addRadialMenuItem(82, 3, "@sui:color_frame");
 	
     WearableObjectMenuComponent::fillObjectMenuResponse(sceneObject, menuResponse, player); 	
 }
 
 int ArmorObjectMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, CreatureObject* player, byte selectedID) const {
 
-	if (selectedID == 81 || selectedID == 82) {
+	if (selectedID == 81) {
 		
 		ManagedReference<SceneObject*> parent = sceneObject->getParent().get();
 	
@@ -88,10 +87,7 @@ int ArmorObjectMenuComponent::handleObjectMenuSelect(SceneObject* sceneObject, C
 		// The Sui Box.
 		ManagedReference<SuiColorBox*> cbox = new SuiColorBox(player, SuiWindowType::COLOR_ARMOR);
 		cbox->setCallback(new ColorArmorSuiCallback(server));
-		if (selectedID == 81)
-			cbox->setColorPalette(variables.elementAt(1).getKey()); // First one seems to be the frame of it? Skip to 2nd.
-		else
-			cbox->setColorPalette(variables.elementAt(0).getKey());
+		cbox->setColorPalette(variables.elementAt(1).getKey()); // First one seems to be the frame of it? Skip to 2nd.
 		cbox->setUsingObject(sceneObject);
 
 		// Add to player.
