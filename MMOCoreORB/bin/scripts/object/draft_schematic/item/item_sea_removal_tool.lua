6266686af20aa3036ac6e9477b974fcd09963b14
c0pp3r <dominguez.joey@gmail.com>
2018-01-29 09:34:43 -0500

    Add in SEA removal tool

---
+++ MMOCoreORB/bin/scripts/object/draft_schematic/item/item_sea_removal_tool.lua
@@ -0,0 +1,35 @@
+object_draft_schematic_item_sea_removal_tool = object_draft_schematic_item_shared_clothing_sea_removal_tool:new {
+
+   templateType = DRAFTSCHEMATIC,
+
+   customObjectName = "SEA Removal Tool",
+
+   craftingToolTab = 524288, -- (See DraftSchematicObjectTemplate.h)
+   complexity = 1,
+   size = 4,
+
+   xpType = "crafting_general",
+   xp = 120,
+
+   assemblySkill = "general_assembly",
+   experimentingSkill = "general_experimentation",
+   customizationSkill = "clothing_customization",
+
+   customizationOptions = {},
+   customizationStringNames = {},
+   customizationDefaults = {},
+
+   ingredientTemplateNames = {"craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n"},
+   ingredientTitleNames = {"assembly_enclosure", "thermal_shielding", "geonosian_power_cube", "clasp"},
+   ingredientSlotType = {0, 0, 1, 1},
+   resourceTypes = {"metal", "petrochem_inert_polymer", "object/tangible/component/weapon/shared_geonosian_power_cube.iff", "object/tangible/slicing/shared_slicing_molecular_clamp.iff"},
+   resourceQuantities = {10, 5, 1, 2},
+   contribution = {100, 100, 100, 100},
+
+   targetTemplate = "object/tangible/item/clothing_sea_removal_tool.iff",
+
+   additionalTemplates = {
+             }
+
+}
+ObjectTemplates:addTemplate(object_draft_schematic_item_sea_removal_tool, "object/draft_schematic/item/clothing_sea_removal_tool.iff")
--- MMOCoreORB/bin/scripts/object/draft_schematic/item/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/item/serverobjects.lua
@@ -117,3 +117,5 @@ includeFile("draft_schematic/item/item_twelve_sided_dice.lua")
 includeFile("draft_schematic/item/item_twenty_sided_dice.lua")
 includeFile("draft_schematic/item/item_weapon_station.lua")
 includeFile("draft_schematic/item/item_weapon_tool.lua")
+--New Tools
+includeFile("draft_schematic/item/item_sea_removal_tool.lua")
