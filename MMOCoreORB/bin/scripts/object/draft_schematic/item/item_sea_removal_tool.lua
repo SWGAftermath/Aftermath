40fa02d32b79ace27ae23501b9d97abbd6469d3b
c0pp3r <dominguez.joey@gmail.com>
2018-01-31 14:57:55 -0500

    add shared_ to armor and clothing

--- MMOCoreORB/bin/scripts/object/draft_schematic/item/item_sea_removal_tool.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/item/item_sea_removal_tool.lua
@@ -22,7 +22,7 @@ object_draft_schematic_item_sea_removal_tool = object_draft_schematic_item_share
    ingredientTemplateNames = {"craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n"},
    ingredientTitleNames = {"assembly_enclosure", "thermal_shielding", "geonosian_power_cube", "clasp", "clothing_attachment", "armor_attachment"},
    ingredientSlotType = {0, 0, 1, 1, 1, 1},
-   resourceTypes = {"metal", "petrochem_inert_polymer", "object/tangible/component/weapon/shared_geonosian_power_cube.iff", "object/tangible/slicing/shared_slicing_molecular_clamp.iff", "object/tangible/gem/clothing.iff", "object/tangible/gem/armor.iff"},
+   resourceTypes = {"metal", "petrochem_inert_polymer", "object/tangible/component/weapon/shared_geonosian_power_cube.iff", "object/tangible/slicing/shared_slicing_molecular_clamp.iff", "object/tangible/gem/shared_clothing.iff", "object/tangible/gem/shared_armor.iff"},
    resourceQuantities = {10, 5, 1, 2, 1, 1},
    contribution = {100, 100, 100, 100, 100, 100},
 
