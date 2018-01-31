f1cb39188c362bb8abec5302b41d7d50e7e7e348
c0pp3r <dominguez.joey@gmail.com>
2018-01-31 14:34:00 -0500

    add CA and AA as ingredients for SEA tool

--- MMOCoreORB/bin/scripts/object/draft_schematic/item/item_sea_removal_tool.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/item/item_sea_removal_tool.lua
@@ -19,12 +19,12 @@ object_draft_schematic_item_sea_removal_tool = object_draft_schematic_item_share
    customizationStringNames = {},
    customizationDefaults = {},
 
-   ingredientTemplateNames = {"craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n"},
-   ingredientTitleNames = {"assembly_enclosure", "thermal_shielding", "geonosian_power_cube", "clasp"},
-   ingredientSlotType = {0, 0, 1, 1},
-   resourceTypes = {"metal", "petrochem_inert_polymer", "object/tangible/component/weapon/shared_geonosian_power_cube.iff", "object/tangible/slicing/shared_slicing_molecular_clamp.iff"},
-   resourceQuantities = {10, 5, 1, 2},
-   contribution = {100, 100, 100, 100},
+   ingredientTemplateNames = {"craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n", "craft_item_ingredients_n"},
+   ingredientTitleNames = {"assembly_enclosure", "thermal_shielding", "geonosian_power_cube", "clasp", "clothing_attachment", "armor_attachment"},
+   ingredientSlotType = {0, 0, 1, 1, 1, 1},
+   resourceTypes = {"metal", "petrochem_inert_polymer", "object/tangible/component/weapon/shared_geonosian_power_cube.iff", "object/tangible/slicing/shared_slicing_molecular_clamp.iff", "object/tangible/gem/clothing.iff", "object/tangible/gem/armor.iff"},
+   resourceQuantities = {10, 5, 1, 2, 1, 1},
+   contribution = {100, 100, 100, 100, 100, 100},
 
    targetTemplate = "object/tangible/item/clothing_sea_removal_tool.iff",
 
