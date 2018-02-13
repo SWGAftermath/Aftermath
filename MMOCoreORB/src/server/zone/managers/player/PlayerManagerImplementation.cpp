01325b01ac572847612145310b193acbeee1681d
c0pp3r <dominguez.joey@gmail.com>
2018-02-13 12:15:47 -0500

    Revert "drop BH mission on jedi clone"
    This reverts commit f461547a9ebf1202018db586bdc85fd2b7c53d14.

--- MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/player/PlayerManagerImplementation.cpp
@@ -103,7 +103,6 @@
 #include "server/zone/objects/building/TutorialBuildingObject.h"
 
 #include "server/zone/managers/visibility/VisibilityManager.h"
-#include "server/zone/managers/mission/MissionManager.h"
 
 
 
@@ -1145,10 +1144,6 @@ void PlayerManagerImplementation::sendPlayerToCloner(CreatureObject* player, uin
 		message.setTO("exp_n", "jedi_general");
 		player->sendSystemMessage(message);
 		VisibilityManager::instance()->clearVisibility(player);
-		MissionManager* missionManager = server->getMissionManager();
-		uint64 id = player->getObjectID();
-		missionManager->removePlayerFromBountyList(id);
-
 	}
 }
 
