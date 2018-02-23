4c80bdca0099db016d26f1d004fc2fc7dcb654ce
c0pp3r <dominguez.joey@gmail.com>
2018-02-23 08:49:34 -0500

    Disarm shot to root player for 10 seconds

--- MMOCoreORB/src/server/zone/objects/creature/commands/DisarmingShot2Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/DisarmingShot2Command.h
@@ -21,8 +21,46 @@ public:
 
 		if (!checkInvalidLocomotions(creature))
 			return INVALIDLOCOMOTION;
+		ManagedReference<SceneObject*> object = server->getZoneServer()->getObject(target);
+		CreatureObject* targetCreature = dynamic_cast<CreatureObject*>(object.get());
 
-		return doCombatAction(creature, target);
+		if (targetCreature == NULL)
+			return INVALIDTARGET;
+
+		Locker clocker(targetCreature, creature);
+
+		ManagedReference<PlayerObject*> player = creature->getPlayerObject();
+		PlayerObject* targetPlayerObject = targetCreature->getPlayerObject();
+
+		if (targetPlayerObject == NULL) {
+			return INVALIDTARGET;
+		} else if (player == NULL)
+			return GENERALERROR;
+		
+		int res = doCombatAction(creature, target);
+
+		if (res == SUCCESS) {
+
+			// Setup debuff.
+
+
+			if (targetCreature != NULL) {
+				Locker clocker(targetCreature, creature);
+
+				ManagedReference<Buff*> buff = new Buff(targetCreature, getNameCRC(), 10, BuffType::OTHER);
+
+				Locker locker(buff);
+
+				buff->setSpeedMultiplierMod(speed);
+				buff->setAccelerationMultiplierMod(speed);
+				targetCreature->setRootedState(10);
+
+				targetCreature->addBuff(buff);
+
+			}
+
+		}
+		return res;
 	}
 
 };
