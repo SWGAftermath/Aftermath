150299bba9a56c3a7540b1ccb5bff3978f7741c6
c0pp3r <dominguez.joey@gmail.com>
2018-03-12 13:23:13 -0400

    another error

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_stormtrooper_chest_crafted.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_stormtrooper_chest_crafted.lua
@@ -70,7 +70,7 @@ object_draft_schematic_clothing_clothing_armor_stormtrooper_chest_crafted = obje
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
-   targetTemplate = "object/tangible/wearables/armor/composite/armor_stormtrooper_chest_plate_crafted.iff",
+   targetTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_chest_plate_crafted.iff",
 
    additionalTemplates = {
              }
