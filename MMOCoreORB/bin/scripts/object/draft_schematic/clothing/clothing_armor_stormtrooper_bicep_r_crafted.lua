c5dc553c0966de01d59b5d6dd41459207d2237a3
c0pp3r <dominguez.joey@gmail.com>
2018-03-14 11:54:19 -0400

    fix typos in schems

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_stormtrooper_bicep_r_crafted.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_armor_stormtrooper_bicep_r_crafted.lua
@@ -70,7 +70,7 @@ object_draft_schematic_clothing_clothing_armor_stormtrooper_bicep_r_crafted = ob
    contribution = {100, 100, 100, 100, 100, 100, 100, 100, 100},
 
 
-   targetTemplate = "object/tangible/wearables/armor/composite/armor_stormtrooper_bicep_r_crafted.iff",
+   targetTemplate = "object/tangible/wearables/armor/stormtrooper/armor_stormtrooper_bicep_r_crafted.iff",
 
    additionalTemplates = {
              }
