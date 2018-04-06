0cbc2fa6a7550b01ffb202fe253af8316824237f
c0pp3r <dominguez.joey@gmail.com>
2018-04-05 22:26:22 -0400

    add player management to csr and ec

--- MMOCoreORB/bin/scripts/staff/levels/ec.lua
+++ MMOCoreORB/bin/scripts/staff/levels/ec.lua
@@ -8,7 +8,8 @@ ec = {
 		"admin_general_admin_01",
 		"admin_general_admin_02",
 		"admin_server_admin_01",
-		"admin_spawn_management_01"
+		"admin_spawn_management_01",
+		"admin_player_management_01"
 	}
 }
 
