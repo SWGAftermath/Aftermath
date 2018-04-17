2df00f495e60e60cfe265283ac47e4f7d12da51c
c0pp3r <dominguez.joey@gmail.com>
2018-04-17 09:36:09 -0400

    remove force power and force regen from clothing

--- MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_jedi_padawan.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_jedi_padawan.lua
@@ -64,8 +64,6 @@ object_tangible_wearables_robe_robe_jedi_padawan = object_tangible_wearables_rob
 				"object/creature/player/zabrak_female.iff" },
 				
 				skillMods = {
-				    {"jedi_force_power_max", 250},
-				    {"jedi_force_power_regen", 10}
 				},
 				noTrade = 1,
 
