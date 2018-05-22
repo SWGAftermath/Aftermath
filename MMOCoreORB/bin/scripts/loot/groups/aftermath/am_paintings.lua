7e5d901eca78bb8214c360876e93634227468458
MIKELAPTOP\Michael <michael.d.hicks@gmail.com>
2018-05-21 22:20:11 -0500

    Correction to AM_Paintings

--- MMOCoreORB/bin/scripts/loot/groups/aftermath/am_paintings.lua
+++ MMOCoreORB/bin/scripts/loot/groups/aftermath/am_paintings.lua
@@ -5,14 +5,14 @@ am_paintings = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "painting_bobafett", weight = 1200000},
-		{itemTemplate = "painting_deathstar", weight = 1200000},
+		{itemTemplate = "painting_bobafett", weight = 1400000},
+		{itemTemplate = "painting_deathstar", weight = 1400000},
 --		{itemTemplate = "painting_emperor", weight = 1400000},
-		{itemTemplate = "painting_tfaset_1", weight = 1200000},
-		{itemTemplate = "painting_tfaset_2", weight = 1200000},
-		{itemTemplate = "painting_tfaset_3", weight = 1200000},
-		{itemTemplate = "painting_tfaset_4", weight = 1200000},
-		{itemTemplate = "painting_silhouette", weight = 1200000}
+		{itemTemplate = "painting_tfaset_1", weight = 1400000},
+		{itemTemplate = "painting_tfaset_2", weight = 1400000},
+		{itemTemplate = "painting_tfaset_3", weight = 1400000},
+		{itemTemplate = "painting_tfaset_4", weight = 1400000},
+		{itemTemplate = "painting_silhouette", weight = 1600000}
 --		{itemTemplate = "painting_xwing01", weight = 1200000}
 	}
 }
