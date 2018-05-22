8c3bef3134c0a026b22751922ced7a47b575b4b5
MIKELAPTOP\Michael <michael.d.hicks@gmail.com>
2018-05-21 21:35:48 -0500

    Lua for Exclusive Painting Gallery

--- MMOCoreORB/bin/scripts/loot/groups/aftermath/am_paintings.lua
+++ MMOCoreORB/bin/scripts/loot/groups/aftermath/am_paintings.lua
@@ -6,13 +6,14 @@ am_paintings = {
 	maximumLevel = 0,
 	lootItems = {
 		{itemTemplate = "painting_bobafett", weight = 1200000},
-		{itemTemplate = "painting_deathstar", weight = 1400000},
-		{itemTemplate = "painting_emperor", weight = 1400000},
+		{itemTemplate = "painting_deathstar", weight = 1200000},
+--		{itemTemplate = "painting_emperor", weight = 1400000},
 		{itemTemplate = "painting_tfaset_1", weight = 1200000},
 		{itemTemplate = "painting_tfaset_2", weight = 1200000},
 		{itemTemplate = "painting_tfaset_3", weight = 1200000},
 		{itemTemplate = "painting_tfaset_4", weight = 1200000},
-		{itemTemplate = "painting_xwing01", weight = 1200000}
+		{itemTemplate = "painting_silhouette", weight = 1200000}
+--		{itemTemplate = "painting_xwing01", weight = 1200000}
 	}
 }
 
