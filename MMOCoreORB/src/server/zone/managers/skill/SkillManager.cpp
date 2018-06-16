7e8b5144ff9d29e474e43b02fa03452f9144e8c2
c0pp3r <dominguez.joey@gmail.com>
2018-06-15 22:02:14 -0400

    [fixed] Functionality to set player rank to 0

--- MMOCoreORB/src/server/zone/managers/skill/SkillManager.cpp
+++ MMOCoreORB/src/server/zone/managers/skill/SkillManager.cpp
@@ -416,7 +416,7 @@ void SkillManager::removeSkillRelatedMissions(CreatureObject* creature, Skill* s
 	}
 }
 
-bool SkillManager::surrenderSkill(const String& skillName, CreatureObject* creature, bool notifyClient) {
+bool SkillManager::surrenderSkill(const String& skillName, CreatureObject* creature, bool notifyClient, bool checkFrs) {
 	Skill* skill = skillMap.get(skillName.hashCode());
 
 	if (skill == NULL)
@@ -494,7 +494,7 @@ bool SkillManager::surrenderSkill(const String& skillName, CreatureObject* creat
 
 		FrsManager* frsManager = creature->getZoneServer()->getFrsManager();
 
-		if (frsManager->isFrsEnabled()) {
+		if (checkFrs && frsManager->isFrsEnabled()) {
 			frsManager->handleSkillRevoked(creature, skillName);
 		}
 
