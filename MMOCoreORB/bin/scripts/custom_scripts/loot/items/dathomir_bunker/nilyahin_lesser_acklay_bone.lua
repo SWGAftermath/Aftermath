87c50a30efc886caeaebb57633587085bcb51c2c
c0pp3r <dominguez.joey@gmail.com>
2018-06-29 08:17:02 -0400

    [added] group for lesser acklay bones on dath

---
+++ MMOCoreORB/bin/scripts/custom_scripts/loot/items/dathomir_bunker/nilyahin_lesser_acklay_bone.lua
@@ -0,0 +1,26 @@
+--Generated for the Aftermath Server by Mindbust
+
+nilyahin_lesser_acklay_bone = {
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Ni'lyahin Stolen Acklay Bone",
+	directObjectTemplate = "object/tangible/component/weapon/acklay_bone_reinforcement_core.iff",
+	craftingValues = {
+		{"mindamage",54,100},
+		{"maxdamage",54,100},
+		{"useCount",1,6,0},
+		{"attackspeed",0,0,0,0},
+		{"woundchance",0,0,0,0},
+		{"hitpoints",0,0,0,0},
+		{"zerorangemod",0,0,0,0},
+		{"maxrangemod",0,0,0,0},
+		{"midrangemod",0,0,0,0},
+		{"attackhealthcost",0,0,0,0},
+		{"attackactioncost",0,0,0,0},
+		{"attackmindcost",0,0,0,0},
+	},
+	customizationStringNames = {},
+	customizationValues = {}
+}
+
+addLootItemTemplate("nilyahin_lesser_acklay_bone", nilyahin_lesser_acklay_bone)
