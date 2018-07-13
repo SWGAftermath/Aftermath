fd8f048787aed68e0604b66ff270e9b747432de1
c0pp3r <dominguez.joey@gmail.com>
2018-07-13 09:33:34 -0400

    [fixed] missing shared_

--- MMOCoreORB/bin/scripts/custom_scripts/object/tangible/veteran_reward/frn_vet_rug_imperial.lua
+++ MMOCoreORB/bin/scripts/custom_scripts/object/tangible/veteran_reward/frn_vet_rug_imperial.lua
@@ -41,7 +41,7 @@
 --this exception also makes it possible to release a modified version 
 
 
-object_tangible_veteran_reward_frn_vet_rug_imperial = object_tangible_veteran_reward_frn_vet_rug_imperial:new {
+object_tangible_veteran_reward_frn_vet_rug_imperial = object_tangible_veteran_reward_shared_frn_vet_rug_imperial:new {
 
 }
 
