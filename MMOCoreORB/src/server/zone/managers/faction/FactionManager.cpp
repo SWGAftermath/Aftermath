9526f8541799dcc07c81717d5125e628c3a258fa
c0pp3r <dominguez.joey@gmail.com>
2018-07-15 18:09:19 -0400

    [fixed] query format

--- MMOCoreORB/src/server/zone/managers/faction/FactionManager.cpp
+++ MMOCoreORB/src/server/zone/managers/faction/FactionManager.cpp
@@ -189,7 +189,7 @@ void FactionManager::awardPvpFactionPoints(TangibleObject* killer, CreatureObjec
 			int killerRating = ghost->getPvpRating();
 			int playerRating = killedGhost->getPvpRating();
 			String winner = "reb";
-			gcwKillQuery << "INSERT INTO gcw_kills(rebel, reb_rating, imperial, imp_rating, winner) VALUES ('" << killerName <<"','" << killerRating << "'," << playerName << ", '" << ", '" << playerRating << winner << "');";
+			gcwKillQuery << "INSERT INTO gcw_kills(rebel, reb_rating, imperial, imp_rating, winner) VALUES ('" << killerName <<"','" << killerRating << "'," << playerName << ", '" << playerRating << ", '" << winner << "');";
 			ServerDatabase::instance()->executeStatement(gcwKillQuery);
 			if (group != NULL){
 				//Locker lockerGroup(group, _this.getReferenceUnsafeStaticCast());
@@ -243,7 +243,7 @@ void FactionManager::awardPvpFactionPoints(TangibleObject* killer, CreatureObjec
 			int killerRating = ghost->getPvpRating();
 			int playerRating = killedGhost->getPvpRating();
 			String winner = "imp";
-			gcwKillQuery << "INSERT INTO gcw_kills(rebel, reb_rating, imperial, imp_rating, winner) VALUES ('" << playerName <<"','" << playerRating << "'," << killerName << ", '" << ", '" << killerRating << winner << "');";
+			gcwKillQuery << "INSERT INTO gcw_kills(rebel, reb_rating, imperial, imp_rating, winner) VALUES ('" << playerName <<"','" << playerRating << "'," << killerName << ", '" << killerRating << ", '" << winner << "');";
 			ServerDatabase::instance()->executeStatement(gcwKillQuery);
 			if (group != NULL){
 				//Locker lockerGroup(group, _this.getReferenceUnsafeStaticCast());
