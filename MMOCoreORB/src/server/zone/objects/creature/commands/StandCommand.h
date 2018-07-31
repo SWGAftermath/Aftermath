40a349da7a2be04a333c2aba1f24e388ae8c02bf
c0pp3r <dominguez.joey@gmail.com>
2018-07-31 08:03:30 -0400

    [fixed] more cleanup

--- MMOCoreORB/src/server/zone/objects/creature/commands/StandCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/StandCommand.h
@@ -17,18 +17,20 @@ public:
 
 	int doQueueCommand(CreatureObject* creature, const uint64& target, const UnicodeString& arguments) const {
 
+
 		if (!checkStateMask(creature)) {
 			return INVALIDSTATE;
 		}
 
 		if (!checkInvalidLocomotions(creature)) {
-			if (creature->isFeigningDeath() && !creature->hasIncapTimer()) {
+			if(creature->isFeigningDeath() && !creature->hasIncapTimer()) {
 				creature->removeFeignedDeath();
 				creature->setPosture(CreaturePosture::KNOCKEDDOWN, false, true);
 				//Allow us to pass the state/locomotion checks below, but still enter dizzy/KD checks
 			} else {
 				return INVALIDLOCOMOTION;
 			}
+
 		}
 
 		if (creature->hasAttackDelay())
@@ -43,7 +45,6 @@ public:
 			} else {
 				creature->setPosture(CreaturePosture::UPRIGHT);
 			}
-
 		}
 
 		return SUCCESS;
