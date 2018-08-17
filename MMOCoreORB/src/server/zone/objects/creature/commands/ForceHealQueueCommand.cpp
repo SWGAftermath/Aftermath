48ba88609b28d590638dd6cd8207ddccd627e423
c0pp3r <dominguez.joey@gmail.com>
2018-08-16 23:33:14 -0400

    Revert "new debugging"
    This reverts commit 891b054c6dd5947f240d687a78d9666eff36ca5e.

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceHealQueueCommand.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceHealQueueCommand.cpp
@@ -102,7 +102,7 @@ int ForceHealQueueCommand::runCommand(CreatureObject* creature, CreatureObject* 
 				int maxHam = targetCreature->getMaxHAM(attrib) - targetCreature->getWounds(attrib);
 				int amtToHeal = maxHam - curHam;
 				if (forceHeal > 0){
-					info("Jedi is in FRS and has a forceHeal value of: " + String::valueOf(forceHeal), true);
+					info("Jedi is in FRS and has a forceHeal value greater than 0 ", true);
 					healAmountFinal = healAmount + (healAmount * (forceHeal / 100));
 				}else{
 					healAmountFinal = healAmount;
