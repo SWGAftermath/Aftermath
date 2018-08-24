a8e4b78f05e57a9ea954b9623ec4502a899b25d5
c0pp3r <dominguez.joey@gmail.com>
2018-08-24 14:41:10 -0400

    [changed] reduced trials to 3 stages

--- MMOCoreORB/bin/scripts/screenplays/jedi/trialData.lua
+++ MMOCoreORB/bin/scripts/screenplays/jedi/trialData.lua
@@ -145,43 +145,7 @@ knightTrialQuests = {
 		trialName = "tusken_raider",
 		trialType = TRIAL_HUNT,
 		huntTarget = "tusken_raider",
-		huntGoal = 19
-	},
-	{
-		trialName = "ancient_bull_rancor",
-		trialType = TRIAL_HUNT,
-		huntTarget = "ancient_bull_rancor",
-		huntGoal = 8
-	},
-	{
-		trialName = "stintaril_prowler",
-		trialType = TRIAL_HUNT,
-		huntTarget = "stintaril_prowler",
-		huntGoal = 8
-	},
-	{
-		trialName = "blurrg_raptor",
-		trialType = TRIAL_HUNT,
-		huntTarget = "blurrg_raptor",
-		huntGoal = 3
-	},
-	{
-		trialName = "enraged_kimogila",
-		trialType = TRIAL_HUNT,
-		huntTarget = "enraged_kimogila",
-		huntGoal = 3
-	},
-	{
-		trialName = "peko_peko_albatross",
-		trialType = TRIAL_HUNT,
-		huntTarget = "peko_peko_albatross",
-		huntGoal = 2
-	},
-	{
-		trialName = "graul_marauder",
-		trialType = TRIAL_HUNT,
-		huntTarget = "graul_marauder",
-		huntGoal = 2
+		huntGoal = 25
 	},
 	{
 		trialName = "light_or_dark",
@@ -192,45 +156,7 @@ knightTrialQuests = {
 		trialType = TRIAL_HUNT_FACTION,
 		rebelTarget = "storm_commando",
 		imperialTarget = "rebel_commando",
-		huntGoal = 47
-	},
-	{
-		trialName = "high_general",
-		trialType = TRIAL_HUNT_FACTION,
-		rebelTarget = "imperial_surface_marshal;imperial_high_general;imperial_general",
-		imperialTarget = "rebel_high_general;rebel_surface_marshal;rebel_general",
-		huntGoal = 22
-	},
-	{
-		trialName = "corvette_officer",
-		trialType = TRIAL_HUNT_FACTION,
-		rebelTarget = "stormtrooper_novatrooper_elite_commander",
-		imperialTarget = "corvette_rebel_rear_admiral",
-		huntGoal = 4
-	},
-	{
-		trialName = "geonosian_bunker_acklay",
-		trialType = TRIAL_HUNT,
-		huntTarget = "geonosian_acklay_bunker_boss",
-		huntGoal = 1
-	},
-	{
-		trialName = "nightsister_elder",
-		trialType = TRIAL_HUNT,
-		huntTarget = "nightsister_elder",
-		huntGoal = 1
-	},
-	{
-		trialName = "kiin_dray",
-		trialType = TRIAL_HUNT,
-		huntTarget = "gaping_spider_recluse_giant_kiin_dray",
-		huntGoal = 1
-	},
-	{
-		trialName = "giant_canyon_krayt",
-		trialType = TRIAL_HUNT,
-		huntTarget = "giant_canyon_krayt_dragon",
-		huntGoal = 1
+		huntGoal = 50
 	},
 }
 
