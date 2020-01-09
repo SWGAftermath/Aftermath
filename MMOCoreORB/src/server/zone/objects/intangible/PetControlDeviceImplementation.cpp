03758657e43fe7c2685309f1fdcb573a2e0bfd86
c0pp3r <dominguez.joey@gmail.com>
2020-01-09 15:46:16 -0500

    Revert "[Changed]AT-XT to only 1 out"
    This reverts commit ed7155b0d72c82bd5be9e5655db14c442bc8b414.

--- MMOCoreORB/src/server/zone/objects/intangible/PetControlDeviceImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/intangible/PetControlDeviceImplementation.cpp
@@ -179,7 +179,7 @@ void PetControlDeviceImplementation::callObject(CreatureObject* player) {
 				const CreatureTemplate* activePetTemplate = object->getCreatureTemplate();
 				info("Pet: " + String::valueOf(activePetTemplate->getTemplateName()), true);
 
-				if (activePetTemplate == NULL || activePetTemplate->getTemplateName() == "at_st" || activePetTemplate->getTemplateName() == "at_xt")
+				if (activePetTemplate == nullptr || activePetTemplate->getTemplateName() == "at_st")
 					continue;
 
 				if (++currentlySpawned >= maxPets) {
@@ -202,11 +202,10 @@ void PetControlDeviceImplementation::callObject(CreatureObject* player) {
 				const CreatureTemplate* activePetTemplate = object->getCreatureTemplate();
 				const CreatureTemplate* callingPetTemplate = pet->getCreatureTemplate();
 
-				if (activePetTemplate == NULL || callingPetTemplate == NULL || activePetTemplate->getTemplateName() != "at_st" || activePetTemplate->getTemplateName() != "at_xt")
+				if (activePetTemplate == nullptr || callingPetTemplate == nullptr || activePetTemplate->getTemplateName() != "at_st")
 					continue;
 
-				if (++currentlySpawned >= maxPets || (activePetTemplate->getTemplateName() == "at_st" && callingPetTemplate->getTemplateName() == "at_st") ||
-						(activePetTemplate->getTemplateName() == "at_xt" && callingPetTemplate->getTemplateName() == "at_xt")) {
+				if (++currentlySpawned >= maxPets || (activePetTemplate->getTemplateName() == "at_st" && callingPetTemplate->getTemplateName() == "at_st")) {
 					player->sendSystemMessage("@pet/pet_menu:at_max"); // You already have the maximum number of pets of this type that you can call.
 					return;
 				}
