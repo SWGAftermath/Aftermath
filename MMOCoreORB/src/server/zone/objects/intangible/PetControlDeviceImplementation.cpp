5a40169216e1ef390eb09b9572fccbcfda5e4067
c0pp3r <dominguez.joey@gmail.com>
2020-01-09 16:01:52 -0500

    [Fixed]Typo

--- MMOCoreORB/src/server/zone/objects/intangible/PetControlDeviceImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/intangible/PetControlDeviceImplementation.cpp
@@ -202,7 +202,7 @@ void PetControlDeviceImplementation::callObject(CreatureObject* player) {
 				const CreatureTemplate* activePetTemplate = object->getCreatureTemplate();
 				const CreatureTemplate* callingPetTemplate = pet->getCreatureTemplate();
 
-				if (activePetTemplate == nullptr || callingPetTemplate == nullptr || (activePetTemplate->getTemplateName() != "at_st" && activePetTemplate->activePetTemplate() != "at_xt"))
+				if (activePetTemplate == nullptr || callingPetTemplate == nullptr || (activePetTemplate->getTemplateName() != "at_st" && activePetTemplate->getTemplateName() != "at_xt"))
 					continue;
 
 				if (++currentlySpawned >= maxPets || ((activePetTemplate->getTemplateName() == "at_st" && callingPetTemplate->getTemplateName() == "at_st") || (activePetTemplate->getTemplateName() == "at_xt" && callingPetTemplate->getTemplateName() == "at_xt"))) {
