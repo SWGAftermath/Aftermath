86afaf4804280646467e985c92ae678bf73f446f
Dirtnose <dirtnose@gmail.com>
2020-01-22 19:57:28 +0000

    [Fixed]Chef Barrels now function.

--- MMOCoreORB/bin/scripts/object/tangible/component/food/container_barrel.lua
+++ MMOCoreORB/bin/scripts/object/tangible/component/food/container_barrel.lua
@@ -49,8 +49,8 @@ object_tangible_component_food_container_barrel = object_tangible_component_food
 	experimentalWeights = {1, 1, 1},
 	experimentalGroupTitles = {"null", "null", "null"},
 	experimentalSubGroupTitles = {"null", "null", "quantity_bonus"},
-	experimentalMin = {0, 0, 1},
-	experimentalMax = {0, 0, 1},
+	experimentalMin = {0, 0, 4},
+	experimentalMax = {0, 0, 4},
 	experimentalPrecision = {0, 0, 0},
 	experimentalCombineType = {0, 0, 1},
 }
