ce7f0285aeb2b6fb9feac7fc9014d457aa12a477
Dirtnose <dirtnose@gmail.com>
2020-01-30 12:13:00 +0000

    [Changed]Removed imp alien faction penalty

--- MMOCoreORB/bin/scripts/managers/gcw_manager.lua
+++ MMOCoreORB/bin/scripts/managers/gcw_manager.lua
@@ -93,15 +93,15 @@ HQValues = {
 --HUMAN = 0; RODIAN = 1; TRANDOSHAN = 2; MONCAL = 3; WOOKIE = 4; BOTHAN = 5; TWILEK = 6; ZABRAK = 7; ITHORIAN = 0x21; SULLUSTAN = 0x31;
 imperial_racial_penalty = {
 	{0, 1},
-	{1, 3}, --rodian
-	{2, 2}, -- trando
-	{3, 3}, -- moncal
-	{4, 3}, -- wookie
-	{5, 2},  -- bothan
-	{6, 1.5}, -- twilek
-	{7, 2},  -- zabrack
-	{33, 2}, --ithorian
-	{49, 2}, -- sullustan
+	{1, 1}, --rodian
+	{2, 1}, -- trando
+	{3, 1}, -- moncal
+	{4, 1}, -- wookie
+	{5, 1},  -- bothan
+	{6, 1}, -- twilek
+	{7, 1},  -- zabrack
+	{33, 1}, --ithorian
+	{49, 1}, -- sullustan
 
 }
 
